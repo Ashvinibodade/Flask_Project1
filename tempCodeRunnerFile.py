bac228091268e9433f21e50564853947a791c632
Ashvinibodade <ashvinibodade234@gmail.com>
2023-07-17 12:48:57 +0530

    first flask project

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+ # if request.method=='POST':
+    #     title = request.form['title']
+    #     desc = request.form['desc']
+    #     todo = Todo(title=title, desc=desc)
+    #     db.session.add(todo)
+    #     db.session.commit()
+        
+    # allTodo = Todo.query.all()
